94e701b642957cbfbb5c637eeee967e40bb4c17b
Matt Bierner <mattbierner@gmail.com>
2014-03-21 14:11:31 -0400

    Removed debugging routine

--- dist_node/compile.js
+++ dist_node/compile.js
@@ -19,7 +19,6 @@ var __o = require("ecma-unparse")["unparse"],
     });
 (compile = (function(input, header, options, ok, err) {
     try {
-        console.log(options);
         return ok(((header || "") + compiler(input, options)));
     } catch (e) {
         return err(e);
